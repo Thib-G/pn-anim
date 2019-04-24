9fdbc2cdf944987348f5c709baa2afa1b97f9265
Thibaut Goelff <thibaut.goelff@infrabel.be>
2019-04-24 18:36:02 +0200

    Update about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <b-row>
+    <b-col>
+      <h1>À propos</h1>
+      <b-card class="mt-2 text-left">
+        <h2>Animation d’un passage à niveau</h2>
+        <p>Cette animation utilise les données SGM (Système de Gestion de la Maintenance)
+          pour visualiser l’aspect d’un passage à niveau (PN) en fonction des variables
+          récoltées</p>
+        <p>Le but est de reconstituer la logique d’un PN pour plus facilement comprendre
+          les données qui sont renvoyées par les systèmes IoT (<em>Internet of Things</em>).</p>
+        <h3>Code source</h3>
+        <p>Le code source est disponible sur
+          <b-link href="https://github.com/Thib-G/pn-anim" target="_blank">GitHub</b-link>.</p>
+        <h3>Auteur</h3>
+          <p>Thibaut Goelff <small class="text-muted">thibaut [at] goelff.be</small></p>
+        <h3>Remerciements</h3>
+          <p>Tous les collègues qui m’ont aidé.</p>
+      </b-card>
+    </b-col>
+  </b-row>
 </template>
